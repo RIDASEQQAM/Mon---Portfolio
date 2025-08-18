7a420d9967a2b73536426ae4235fcaaa3a68ef24
HVMG1124 <rida.seqqam@orange.com>
2025-08-18 14:01:52 +0100

    chore: update deployment configuration for Vercel

--- vite.config.ts
+++ vite.config.ts
@@ -4,4 +4,20 @@ import react from "@vitejs/plugin-react";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    outDir: "dist",
+    sourcemap: false,
+    commonjsOptions: {
+      transformMixedEsModules: true
+    },
+    chunkSizeWarningLimit: 1600,
+    rollupOptions: {
+      output: {
+        manualChunks: {
+          vendor: ["react", "react-dom"],
+          three: ["three", "@react-three/fiber", "@react-three/drei"]
+        }
+      }
+    }
+  }
 });
