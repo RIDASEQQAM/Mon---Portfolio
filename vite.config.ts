98b30e4ac13916edb88366498771871505e52272
HVMG1124 <rida.seqqam@orange.com>
2025-08-18 11:35:11 +0100

    making some adjustments to project config

--- vite.config.ts
+++ vite.config.ts
@@ -4,4 +4,18 @@ import react from "@vitejs/plugin-react";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    outDir: 'dist',
+    rollupOptions: {
+      output: {
+        manualChunks: {
+          'react-vendor': ['react', 'react-dom'],
+          'three-vendor': ['three', '@react-three/fiber', '@react-three/drei'],
+        },
+      },
+    },
+  },
+  optimizeDeps: {
+    include: ['react', 'react-dom', 'three'],
+  },
 });
